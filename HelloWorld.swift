bea5c07a743d38bae95408999742a9e769336f3f
Spencer S <spencer.scarlet@stu.ocsb.ca>
2024-02-09 01:24:34 +0000

    Created HelloWorld.swift

---
+++ HelloWorld.swift
@@ -0,0 +1,21 @@
+//
+
+//  HelloWorld.swift
+
+//
+
+//  Created by Spencer Scarlett
+
+//  Created on 2024-Feb-08
+
+//  Version 1.0
+
+//  Copyright (c) 2024 Spencer Scarlett. All rights reserved.
+
+//
+
+//  Simple Swift program to test the Style Checkers and some other things
+
+// Print statements
+print("Hi Chris Dibert")
+print("Hello World!")
